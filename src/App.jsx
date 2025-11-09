4d2b25c5ca9996a924ddfab6ec999c94ae3abbfe
HarshitRaj2712 <panditjee2712@gmail.com>
2025-11-09 21:49:45 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() { 
+  return (
+    <div className="App">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+export default App;
